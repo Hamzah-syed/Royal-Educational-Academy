32030558055ea5b82958a37cfb1541c26ae9776a
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2020-08-10 17:58:47 +0500

    code fixed

--- pages/index.php
+++ pages/index.php
@@ -1,10 +1,10 @@
 <?php
 //connection
 session_start();
-include("./database_connection.php");
+include("../database_connection.php");
 
 if (isset($_SESSION["rolesId"])) {
-  header("Location:./pages/dashboard.php");
+  header("Location:./dashboard.php");
 }
 
 if (isset($_POST['loginSubmit'])) {
@@ -60,11 +60,11 @@ if (isset($_POST['loginSubmit'])) {
 
 
   <!-- Custom fonts for this template-->
-  <link href="./assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="./assets/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="../assets/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
